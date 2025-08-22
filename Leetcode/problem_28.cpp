d4add5bf64b465df5d8aed51c55b4d33ae147324
Abdul Rehman Gulshan <sabdulrehmangulshan@gmail.com>
2025-08-22 20:07:51 +0500

    added new comment

--- Leetcode/problem_28.cpp
+++ Leetcode/problem_28.cpp
@@ -3,7 +3,7 @@
 Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 */
 
-// solved by kmp algorithm O(n+m)
+// solved by kmp algorithm O(n+m) called KMP Pattern Search
 // created LPS finding function
 vector<int> lps_find(string s)
 {
